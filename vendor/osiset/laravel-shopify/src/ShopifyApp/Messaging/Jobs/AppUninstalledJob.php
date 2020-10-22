ff51bf9f524c577d0a0fed8c670ec9a3124e0884
anwar03 <anwarazim03@gmail.com>
2020-10-22 21:25:32 +0600

    pull main

---
+++ vendor/osiset/laravel-shopify/src/ShopifyApp/Messaging/Jobs/AppUninstalledJob.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Osiset\ShopifyApp\Messaging\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Osiset\ShopifyApp\Actions\CancelCurrentPlan;
+use Osiset\ShopifyApp\Contracts\Commands\Shop as IShopCommand;
+use Osiset\ShopifyApp\Contracts\Objects\Values\ShopDomain;
+use Osiset\ShopifyApp\Contracts\Queries\Shop as IShopQuery;
+use stdClass;
+
+/**
+ * Webhook job responsible for handling when the app is uninstalled.
+ */
+class AppUninstalledJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * The shop domain.
+     *
+     * @var ShopDomain
+     */
+    protected $domain;
+
+    /**
+     * The webhook data.
+     *
+     * @var object
+     */
+    protected $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param ShopDomain $domain  The shop domain.
+     * @param stdClass   $data   The webhook data (JSON decoded).
+     *
+     * @return void
+     */
+    public function __construct(ShopDomain $domain, stdClass $data)
+    {
+        $this->domain = $domain;
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param IShopCommand      $shopCommand             The commands for shops.
+     * @param IShopQuery        $shopQuery               The querier for shops.
+     * @param CancelCurrentPlan $cancelCurrentPlanAction The action for cancelling the current plan.
+     *
+     * @return bool
+     */
+    public function handle(
+        IShopCommand $shopCommand,
+        IShopQuery $shopQuery,
+        CancelCurrentPlan $cancelCurrentPlanAction
+    ): bool {
+        // Get the shop
+        $shop = $shopQuery->getByDomain($this->domain);
+        $shopId = $shop->getId();
+
+        // Cancel the current plan
+        $cancelCurrentPlanAction($shopId);
+
+        // Purge shop of token, plan, etc.
+        $shopCommand->clean($shopId);
+
+        // Soft delete the shop.
+        $shopCommand->softDelete($shopId);
+
+        return true;
+    }
+}
